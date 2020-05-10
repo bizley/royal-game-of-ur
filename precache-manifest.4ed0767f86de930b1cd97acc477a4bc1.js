597ee5a9d646cca65b7404112e37ff530481efd1
Bizley <pawel@positive.codes>
2020-05-10 16:49:02 +0200

    Updates

---
+++ precache-manifest.4ed0767f86de930b1cd97acc477a4bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2479eec3e004de91512f846433084e",
+    "url": "/royal-game-of-ur/index.html"
+  },
+  {
+    "revision": "4abf8b5855908e2e5d82",
+    "url": "/royal-game-of-ur/static/css/main.0e47666f.chunk.css"
+  },
+  {
+    "revision": "a559fbe947309a128ac1",
+    "url": "/royal-game-of-ur/static/js/2.96d528ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/royal-game-of-ur/static/js/2.96d528ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abf8b5855908e2e5d82",
+    "url": "/royal-game-of-ur/static/js/main.bfacf2d3.chunk.js"
+  },
+  {
+    "revision": "56d8df944f66aaa064f6",
+    "url": "/royal-game-of-ur/static/js/runtime-main.61d581ac.js"
+  }
+]);
