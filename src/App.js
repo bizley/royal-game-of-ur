db32a2dc00b6a41022a7953bcc6ce703128b46d5
Bizley <pawel@positive.codes>
2020-05-11 08:11:50 +0200

    Added link

--- src/App.js
+++ src/App.js
@@ -88,6 +88,7 @@ function App() {
                 </tr>
                 </tbody>
             </table>
+            <a href="https://en.wikipedia.org/wiki/Royal_Game_of_Ur" target="_blank">Royal Game of Ur at Wiki</a>
         </div>
     );
 }
